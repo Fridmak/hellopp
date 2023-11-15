79c3ec32d40db258ba63890c512083f69714bdb3
black <black@example.com>
2023-11-16 00:45:49 +0500

    greet black

--- HelloApp/Program.cs
+++ HelloApp/Program.cs
@@ -1,2 +1,8 @@
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("hello everyone");
+void GreetWhite()
+{
+    Console.WriteLine("hello white");
+}
+void GreetBlack()
+{
+    Console.WriteLine("hello black");
+}
